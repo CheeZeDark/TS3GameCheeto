9ea79a07ad9561a56cf7953c430a2bf468385fa4
CheeZe Dark(Rikko Matsumato) <110032482+CheeZeDark@users.noreply.github.com>
2025-09-05 12:17:55 +0300

    Add files via upload

---
+++ TS3GameCheeto/dllmain.cpp
@@ -0,0 +1,29 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "includes.hpp"
+
+DWORD WINAPI Thread() {
+    AllocateConsole();
+    cout << "Welcome to my new Cheeto for The Sims 3... Created By CheeZeDark(Rikko Matsumato)" << endl;
+    World_SetTestingCheatsEnabledImpl_t SetTestingCheats = *(World_SetTestingCheatsEnabledImpl_t*)TS3_Offset::ps3_settestcheat;
+    SetTestingCheats();
+    cout << "Success" << endl;
+    return TRUE;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Thread, 0, 0, 0);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ TS3GameCheeto/includes.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+typedef bool(WINAPI* World_SetTestingCheatsEnabledImpl_t)();
+namespace TS3_Offset {
+    uintptr_t GetMod() {
+        return (uintptr_t)GetModuleHandleA("TS3W.exe");
+    }
+    uintptr_t ps3_settestcheat = GetMod() + 0x01162C04; //Our Real .data Offset
+}
+namespace MH {
+#include <MinHook.h>
+}
+
+void AllocateConsole() {
+    FILE* fp;
+    AllocConsole();
+    freopen_s(&fp, "CONOUT$", "w", stdout);
+    SetConsoleTitleA("TS3GameCheeto by CheeZeDark(Rikko Matsumato)");
+}
